5a25457e6817212b780db2d52b2f9fc7c7ddf29c
bigricedumpling <67448111+bigricedumpling@users.noreply.github.com>
2020-06-27 19:35:06 +0800

    master

--- pp55/sketch.js
+++ pp55/sketch.js
@@ -1,7 +1,42 @@
+"use strict";
+let capture;
 
 function setup() {
-  createCanvas(800, 800);
+	createCanvas(windowWidth, windowHeight);
+	capture = createCapture(VIDEO);
+	capture.hide();
+	capture.size(1024, 768);
 }
+
+
 function draw() {
-  background(200);
+	let mainColor = '#2C3A47';
+	let bgColor = '#CAD3C8';
+	background(bgColor);
+	noStroke();
+	fill(mainColor);
+	
+	if (capture.width > 0) {
+		let img = capture.get(0, 0, capture.width, capture.height);
+		img.loadPixels();
+
+		const step = 15;
+		for (var y = step; y < img.height; y += step) {
+			for (var x = step; x < img.width; x += step) {
+				const darkness = getPixelDarknessAtPosition(img, x, y);
+				const radius = 20 * darkness;
+				let sX = x * width / img.width;
+				let sY = y * height / img.height;
+				circle(sX, sY, radius);
+			}
+		}
+	}
 }
+
+//ignore this, initially
+function getPixelDarknessAtPosition(img, x, y) {
+	const mirroring = true;
+	var i = y * img.width +  (mirroring ? (img.width - x - 1) : x);
+	return (255 - img.pixels[i * 4]) / 255;
+}
+
